aa4ca4f292db91288fba23e7acf33ca3899954fc
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-09 15:40:50 -0400

    com 5

--- Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment.js
+++ Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment.js
@@ -1,12 +1,14 @@
 /*
 Miss Jessica Lynne Lewis
-SDI
+SDI MDV2330-O
 Week 1 Output Assignment
  */
 
-/*declaring basic variables used throughout this script*/
+/*declaring basic string and boolean variables used throughout this script*/
 var welcomeGreet;            //variable to greet user
 welcomeGreet = "Welcome";    //variable states welcome to the user
+var goodBye;                 // declaring a variable for goodbye
+goodBye = "Thank you for your input. Goodbye!"; // string for a departure greeting
 var y;                       //variable for true boolean or yes
 y = true;                    //true boolean
 var n;                       //variable for false or no boolean
@@ -17,33 +19,34 @@ var firstName;                                           //declaring the variabl
 firstName = prompt("What is your first name?");          //using the declared variable to prompt user input
 alert(welcomeGreet + firstName + ".My name is Jessica"); //using the greeting variable + the users inputted name variable
 
-var lastName;
-lastName = prompt("What is your last name," + firstName + "?");
-alert("I like your name," + firstName + lastName + "." + "My last name is Lewis");
+var lastName; //declaring the variable for user's last name
+lastName = prompt("What is your last name," + firstName + "?");//prompting the user to enter their last name
+alert("I like your name," + firstName + lastName + "." + "My last name is Lewis.");
+// telling the user my last name while including the variables for their first and last name that they input
 
 /* asking the user random questions */
-var myCar;
-myCar =prompt("Do you have a car? Y/n");
-console.log( "I drive a Honda Civic");
+var myChild;                                                  //declaring the variable for the users child(ren)
+myChild =prompt("Do you have a child or children? Enter y/n");// prompting the user to input if they have children or not
+alert( "Thank you! I have a 7yr old daughter.");              //telling the user that I have a child
 
-var carYear;
-carYear = prompt("What year is your car?");
-var mycarYear;
-mycarYear = 2005;
-console.log("Interesting! My car is a" + myCaryear + ".");
+var carYear;                                         //declaring variable for the users vehicle
+carYear = prompt("What is the year of your vehicle");//prompting the user to input the year of their car
+var myCar;                                           // declaring the variable for my own car
+myCar = 2005;                                        //assigning the number year of my car to the variable
+alert("Interesting! My car is a" + myCar + ".");     //telling the user what the year of my my car is
 
 
 /* A little info about me */
-var faveColour;
-faveColour = "grey";
-var yearofBirth;
-yearofBirth = 1982;
-var parentMom;
-parentMom = "single mom";
-var myJob;
-myJob = "Freelance Web Dev";
-var myOccupation;
-myOccupation = "Student";
+var faveColour; //declaring the variable for a favourite colour
+faveColour = "grey";//assigning the colour value to the variable
+var yearofBirth; //declaring the variable for my year of birth
+yearofBirth = 1982;//assinging a number to the variable for my year of birth
+var myJob;//declaring the variable for my job
+myJob = "Freelance Web Dev"; //assigning the variable my job
+var myOccupation;//declaring the variable for my daily occupation
+myOccupation = "Student";//assigning the variable for my occupation
+alert ("Here is a little about myself. I am a" + myJob +
+    ". I was born in" + yearofBirth + "and my favourite colour is" + faveColour + "."); //Concatenated string with the above listed variable to output to the user
 
-console.log ("Here is a little about myself. I am a" + parentMom + "and" + myJob + "."
-    + "I was born in" + yearofBirth + "and my favourite colour is" + faveColour + ".");
+/* Goodbye message */
+console.log("This concludes todays conversation!" + goodBye); //displaying the goodbye message to the user in the console versus the alert popup
