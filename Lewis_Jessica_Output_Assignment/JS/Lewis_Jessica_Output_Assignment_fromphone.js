32598d3769bf8004adcf24cd8b497bed0b076fc7
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-09 15:43:47 -0400

    testing over

--- Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment_fromphone.js
+++ Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment_fromphone.js
@@ -1,56 +1,49 @@
 
+//declaring basic variables used throughout this script
+var welcomeGreet;
+welcomeGreet = "Welcome";
+var y;
+y = true;
+var n;
+n = false;
+
+
 var firstName;
 firstName = prompt("What is your first name?");
-firstName == true;
-console.log("Welcome" + firstName + "." + "My name is Jessica");
+alert(welcomeGreet + firstName + "." + "My name is Jessica");
 
 
 var lastName;
 lastName = prompt("What is your last name," + firstName + "?");
-lastName == true;
-console.log("I like your name," + firstName + lastName + ". My last name is Lewis");
+alert("I like your name," + firstName + lastName + "." + "My last name is Lewis");
 
 var myCar;
 myCar =prompt("Do you have a car? Y/n");
-myCar == true;
  console.log( "I drive a Honda Civic");
  
  
 var carYear;
-carYear = prompt("What year is your car?")
-carYear == true;
+carYear = prompt("What year is your car?");
 var myCaryear;
 myCaryear = 2005;
 console.log( carYear + "isn't very old. My car is a" + myCaryear + ".");
 
 
-var aboutMe;
-aboutMe = prompt( "I am new to JavaScript";
 
-
+var faveColour;
+faveColour = "grey";
+var yearofBirth;
+yearofBirth = 1982;
 var parentMom;
-parentMom == true;
-parentMom = console.log("Yes I am a mom.");
-
-
-var isStudent;
-isStudent = console.log("I am a student at Full Sail.");
-isStudent = true;
-
-
-var isEmployed;
-isEmployed = false;
-isEmployed = console.log("I am not employed.");
-
-
+parentMom = "single mom";
+var myJob;
+myJob = "Freelance Web Dev";
 var myOccupation;
-myOccupation = "Student, single mom, and Freelance Web Dev";
+myOccupation = "Student";
+console.log ("Here is a little about myself. I am a" + parentMom + "and" + myJob + "."
+    + "I was born in" + yearofBirth + "and my favourite colour is" + faveColour + ".");
 
-var yearOfbirth;
-yearOfbirth= 1982;
 
-var birthLocation;
-birthLocation= "Mainz, Germany";
 
 
 
