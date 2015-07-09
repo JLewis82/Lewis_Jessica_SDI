1066453476e036c35bee5c462ef0220ab46cde0a
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-09 00:00:03 -0400

    commit 3

--- Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment.js
+++ Lewis_Jessica_Output_Assignment/JS/Lewis_Jessica_Output_Assignment.js
@@ -1,60 +1,51 @@
+/*
+Miss Jessica Lynne Lewis
+SDI
+Week 1 Output Assignment
+ */
 
-var firstName;
-firstName = prompt("What is your first name?");
-firstName == true;
-console.log("Welcome" + firstName + "." + "My name is Jessica");
+/*declaring basic variables used throughout this script*/
+var welcomeGreet;            //variable to greet user
+welcomeGreet = "Welcome";    //variable states welcome to the user
+var y;                       //variable for true boolean or yes
+y = true;                    //true boolean
+var n;                       //variable for false or no boolean
+n = false;                   //false boolean
+
+/*getting to know the user*/
+var firstName;                                           //declaring the variable
+firstName = prompt("What is your first name?");          //using the declared variable to prompt user input
+alert(welcomeGreet + firstName + ".My name is Jessica"); //using the greeting variable + the users inputted name variable
 
 
 var lastName;
 lastName = prompt("What is your last name," + firstName + "?");
-lastName == true;
-console.log("I like your name," + firstName + lastName + ". My last name is Lewis");
+alert("I like your name," + firstName + lastName + "." + "My last name is Lewis");
 
+/* asking the user random questions */
 var myCar;
 myCar =prompt("Do you have a car? Y/n");
-myCar == true;
- console.log( "I drive a Honda Civic");
- 
- 
+console.log( "I drive a Honda Civic");
+
+
 var carYear;
-carYear = prompt("What year is your car?")
-carYear == true;
+carYear = prompt("What year is your car?");
 var myCaryear;
 myCaryear = 2005;
 console.log( carYear + "isn't very old. My car is a" + myCaryear + ".");
 
 
-var aboutMe;
-aboutMe = prompt( "I am new to JavaScript";
 
-
+var faveColour;
+faveColour = "grey";
+var yearofBirth;
+yearofBirth = 1982;
 var parentMom;
-parentMom == true;
-parentMom = console.log("Yes I am a mom.");
-
-
-var isStudent;
-isStudent = console.log("I am a student at Full Sail.");
-isStudent = true;
-
-
-var isEmployed;
-isEmployed = false;
-isEmployed = console.log("I am not employed.");
-
-
+parentMom = "single mom";
+var myJob;
+myJob = "Freelance Web Dev";
 var myOccupation;
-myOccupation = "Student, single mom, and Freelance Web Dev";
+myOccupation = "Student";
 
-var yearOfbirth;
-yearOfbirth= 1982;
-
-var birthLocation;
-birthLocation= "Mainz, Germany";
-
-
-
-
-
-
-
+console.log ("Here is a little about myself. I am a" + parentMom + "and" + myJob + "."
+    + "I was born in" + yearofBirth + "and my favourite colour is" + faveColour + ".");
