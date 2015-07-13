3a087ea6ea3100a4a8ccce816a952a76d414ab46
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-13 00:24:50 -0400

    1

--- Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
+++ Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
@@ -3,4 +3,40 @@ SDI MDV2330-O
 Expressions Assignment
  */
 
-//my javascript calculator calculates what?????
+/* magic trick 1 */
+
+//step 1 user enters a number
+var a;
+a = prompt("please enter a number");
+
+//step 2 double the user mumber var a
+var b;
+b =  2;
+var bTotal;
+bTotal= (a * b);
+
+alert(bTotal);
+
+//step 3 is add 10 to bTotal
+var c;
+c = 10;
+var cTotal;
+cTotal = (bTotal + c);
+
+alert(cTotal);
+
+//step 4is halving cTotal so divide by two
+var d;
+d = 2;
+var dTotal;
+dTotal = (cTotal / 2);
+
+//step five is Take away you original number.
+//prompt("Subtract your original number")
+
+var e;
+e = (dTotal - a);
+
+    confirm("is" + "" + e + "" + "your magic number?" );
+
+
