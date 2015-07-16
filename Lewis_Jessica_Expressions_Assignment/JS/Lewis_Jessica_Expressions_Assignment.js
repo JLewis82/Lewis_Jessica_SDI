5cddc541105f583678cd8daa19cf0969a3c34d2a
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-16 14:39:03 -0400

    com 7 frm macbook

--- Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
+++ Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
@@ -4,62 +4,73 @@ Expressions Assignment
 7/15/2015
  */
  
-//basic variables
-var y; //declaring a variable for yes
-y = true; //assigning true to the variable 
-var n; //declaring a variable for no
-n = false; //assigning false to the variable 
-var welCome; //declaring a variable for the greeting 
-welCome = alert("Welcome to my magic script! Errr, I mean magic TRICK!"); //assigning a string to the variable 
+/* the following are basic variables used throughout the script */
+var y;                            //declaring a variable for yes
+y = true;                         //assigning true to the variable
+var n;                            //declaring a variable for no
+n = false;                        //assigning false to the variable
+var welCome;                      //declaring a variable for the greeting
+welCome = alert("Welcome to my magic script! " +
+    "Errr, I mean magic TRICK!"); //assigning a string to the variable
 
 /* ask user to input name */
-var yourName; //declaring a variable for the users name
-yourName = prompt("First, tell me your name so I know how to address you."); //assigning a prompt to the variable to ask the user for their name
-alert("Thank you," + "" + yourName + ".");//using a pop up alert to thank the user 
+var yourName;                               //declaring a variable for the users name
+yourName = prompt("First, tell me your " +
+    "name so I know how to address you."); //assigning a prompt to the variable to ask the user for their name
+alert("Thank you," + "" + yourName + "."); //using a pop up alert to thank the user
+
 
 /* ask user if they would like to learn magic trick 1 */
-var magic; //declaring a variable
-magic = prompt("Today, I will be teaching you some simple math magic to WOW you! " + "Would you like to continue with our first trick" + "" + yourName + "? y/n") ; //assigning the variable a prompt with a concatenated string 
-if(magic = y)//using if/else to confirm
+var magic;                                              //declaring a variable
+magic = prompt("Today, I will be teaching you some simple math magic " +
+    "to WOW you! " + "Would you like to continue " +
+    "with our first trick" + "" + yourName + "? y/n") ; //assigning the variable a prompt with a concatenated string
+
+if(magic = y)                                           //using if/else to confirm
 {
     alert("ok then lets begin...");
 }
 else
 {
-    prompt("sorry but this is not an option today! please kindly click ok")//if the user reply with no 
+    prompt("sorry but this is not an option today! please kindly click ok");    //if the user reply with no
 }
 
-/* explanation of the trick */
-var explainOne; //declaring a variable
-explainOne = alert("This particular magic trick will return the number 5 for any given number you will choose." +
-    " Regardless of your chosen magic number the number 5 will ALWAYS be the end result of this funky math magic trick.") ; //explaining the math magic trick to the use
+/* explanation of the math magic trick */
+var explainOne;                                                                 //declaring a variable
+explainOne = alert("This particular magic trick will return the number 5 for " +
+    "any given number you will choose." +
+    " Regardless of your chosen magic number the " +
+    "number 5 will ALWAYS be the end result of this funky math magic trick.") ; //explaining the math magic trick to the user
 
 
-/* step 1 user enters a number */
-var a; //declaring a variable
-a = prompt("So here is where you enter a number. It can be any number of your liking but beware! " +
-    "You will be using this number to do some math so choose something you can calculate easily."); //asking the user to input a number 
+/* step 1 the user enters a number */
+var a;                                                //declaring a variable
+a = prompt("So here is where you enter a number." +
+    " It can be any number of your liking but beware! " +
+    "You will be using this number to do some math " +
+    "so choose something you can calculate easily."); //asking the user to input a number
+
 
 /* step 2 prompt user to double their number */
-var b; //declaring a variable
-b =  2;// assigning the number 2
-alert("Thank you. Now double your number. Press ok to continue...");//asking user to double the number
-var bTotal;//declaring a variable
-bTotal= (a * b);// taking the variable a and multiplying it by 2
+var b;                                                              // declaring a variable
+b =  2;                                                             // assigning the number 2
+alert("Thank you. Now double your number. Press ok to continue...");// asking user to double the number
+var bTotal;                                                         // declaring a variable
+bTotal= (a * b);                                                    // taking the variable a and multiplying it by 2
 
 /* step 3 prompt user to add 10 to the previous number */
-var c;
-c = 10;
-alert("Thank you. Now add 10 to this. Press ok to continue...");
-var cTotal;
-cTotal = (bTotal + c);
+var c;                                                           // declaring the variable
+c = 10;                                                          // assigning the variable the number 10
+alert("Thank you. Now add 10 to this. Press ok to continue..."); // asking the user to add 10 to the previous number
+var cTotal;                                                      // declaring a variable for the total
+cTotal = (bTotal + c);                                           // assinging the previous total in step 2 plus the 10 assigned to var c
 
 /*step 4 prompt user to halving cTotal so divide by two */
-var d;
-d = 2;
-alert("Now take this number and divide it in half. Press ok to continue...");
-var dTotal;
-dTotal = (cTotal / 2);
+var d;                                                                        // declaring the variable
+d = 2;                                                                        // assigning the number 2 to the variable
+alert("Now take this number and divide it in half. Press ok to continue..."); // asking the user to divide the previous number total by 2
+var dTotal;                                                                   // declaring a variable
+dTotal = (cTotal / 2);                                                        // variable to take the previous total and dividing it by 2
 
 /* step five is prompt user to Take away their original number. */
 
