f7ed3e2c8968a8002046f66c83edef8d518cdc39
Jlewis82 <emeraldkitten82@gmail.com>
2015-07-15 21:01:25 -0400

    Com 6 frm android adding comments

--- Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
+++ Lewis_Jessica_Expressions_Assignment/JS/Lewis_Jessica_Expressions_Assignment.js
@@ -3,36 +3,36 @@ SDI MDV2330-O
 Expressions Assignment
 7/15/2015
  */
-
-/* basic variables */
-var y;
-y = true;
-var n;
-n = false;
-var welCome;
-welCome = alert("Welcome to my magic script! Errr, I mean magic TRICK!");
+ 
+//basic variables
+var y; //declaring a variable for yes
+y = true; //assigning true to the variable 
+var n; //declaring a variable for no
+n = false; //assigning false to the variable 
+var welCome; //declaring a variable for the greeting 
+welCome = alert("Welcome to my magic script! Errr, I mean magic TRICK!"); //assigning a string to the variable 
 
 /* ask user to input name */
-var yourName;
-yourName = prompt("First, tell me your name so I know how to address you.");
-alert("Thank you," + "" + yourName + ".");
+var yourName; //declaring a variable for the users name
+yourName = prompt("First, tell me your name so I know how to address you."); //assigning a prompt to the variable to ask the user for their name
+alert("Thank you," + "" + yourName + ".");//using a pop up alert to thank the user 
 
 /* ask user if they would like to learn magic trick 1 */
-var magic;
-magic = prompt("Today, I will be teaching you some simple math magic to WOW you! " + "Would you like to continue with our first trick" + "" + yourName + "? y/n");
-if(magic = y)
+var magic; //declaring a variable
+magic = prompt("Today, I will be teaching you some simple math magic to WOW you! " + "Would you like to continue with our first trick" + "" + yourName + "? y/n") ; //assigning the variable a prompt with a concatenated string 
+if(magic = y)//using if/else to confirm
 {
     alert("ok then lets begin...");
 }
 else
 {
-    prompt("sorry but this is not an option today! please kindly click ok")
+    prompt("sorry but this is not an option today! please kindly click ok")//if the user reply with no 
 }
 
 /* explanation of the trick */
-var explainOne;
+var explainOne; //declaring a variable
 explainOne = alert("This particular magic trick will return the number 5 for any given number you will choose." +
-    " Regardless of your chosen magic number the number 5 will ALWAYS be the end result of this funky math magic trick!");
+    " Regardless of your chosen magic number the number 5 will ALWAYS be the end result of this funky math magic trick.") ; //explaining the math magic trick to the user
 
 
 /* step 1 user enters a number */
