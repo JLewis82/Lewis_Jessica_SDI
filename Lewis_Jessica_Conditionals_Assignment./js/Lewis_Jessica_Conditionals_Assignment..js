d71726810a30dcd845708d2cae7dadd4e8fab849
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-18 22:57:04 -0400

    com 2 from macbook

--- Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
+++ Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
@@ -38,22 +38,22 @@ theTemp = prompt(yourName + "," + "Please enter the current temperature in numer
 
 if (theTemp <= 30)
 {
-    console.log("Wow! now that is cold!");
+    console.log("Wow! Seems to be a bit past cold for this time of year! Dont forget your sweater!");
 }
 
 if ((theTemp > 30) && (theTemperature <= 55))
 {
-    console.log("It is a cool day");
+    console.log("Seemes like today is a fairly nice and cool day");
 }
 
 if ((theTemp > 55) && (theTemperature <= 75))
 {
-    console.log = ("It is a fairly warm day");
+    console.log = ("It is a nice and fairly warm day. Lets not forget your sunglasses!");
 }
 
 if ((theTemp > 75))
 {
-    console.log("Wow! It is very hot today!");
+    console.log("Wow! It is very hot today! Lets try to loiter around the AC at work today!");
 }
 
 /* The weather using ternary conditional if/else */
@@ -66,4 +66,49 @@ if (theTemp != y)
 } else {
     console.log("It is " + (theTemp >= 90 ? "HOT!" : (theTemperature > 50 ? "NICE!" : "COLD!")))
 }
+
+
+
+
+/* declaring variables for colours and assigning their psychological meaning to them */
+var orange;
+orange = alert();
+var purple;
+purple = ("Purple is the color of the imagination. It can be creative and individual or immature and impractical.");
+
+var green;
+green = alert("Green is the color of balance and growth. It can mean both self-reliance as a positive and possessiveness as a negative, among many other meanings. ");
+
+var brown;
+brown = alert("The color brown is a friendly yet serious, down-to-earth color that relates to security, protection, comfort and material wealth. ");
+
+var blue;
+blue = alert("Blue is the color of trust and peace. It can suggest loyalty and integrity as well as conservatism and frigidity.  ");
+
+var red;
+red = alert("Red is the color of energy, passion, action, ambition and determination. It is also the color of anger and sexual passion.");
+
+var yellow;
+yellow= alert ("yellow is the color of the mind and the intellect. It is optimistic and cheerful. However it can also suggest impatience, criticism and cowardice.");
+
+var white;
+white = alert("White is color at its most complete and pure, the color of perfection. The color meaning of white is purity, innocence, wholeness and completion. ");
+
+var black;
+black = alert("Black is the color of the hidden, the secretive and the unknown, creating an air of mystery. It keeps things bottled up inside, hidden from the world. ");
+
+var gray;
+gray = alert("From a color psychology perspective, gray is the color of compromise - being neither black nor white, it is the transition between two non-colors. It is unemotional and detached and can be indecisive.");
+
+
+/* having user enter fave colour */
+var faveColour;
+faveColour = prompt("Now lets see what you will wear today by you entering your favourite colour! But...." +
+    "please only enter one of the folowing colours: orange, purple, green, blue, red, white, black, gray, or yellow and press enter." + "" + yourName + "."  +
+    "so we can make this decision quite quickly bc we cant have you being late for work today! ");
+
+
+
+/* goodbye */
+
 alert("Thank you for participating today," + "" + yourName + "" + goodBye + ".");
