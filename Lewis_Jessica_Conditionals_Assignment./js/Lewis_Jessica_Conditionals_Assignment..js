4d0a9628106bf4ae9814f52768737a8e4fe5bceb
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-20 21:04:32 -0400

    com 4 from macbook
    testing script using else if()

--- Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
+++ Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
@@ -2,14 +2,12 @@
  * Created by Miss_Jessica_Lynne on 7/17/15.
  */
 /* basic variables */
-var y;
-y = true;
-var n;
-n = false;
+var yes;
+yes = true;
+var no;
+no = false;
 var welCome;
 welCome = alert("Welcome to todays weather! We will review your daily forcast to make sure you are prepared for your day!");
-var goodBye;
-goodBye = alert("Thank you for your time and I hope this information was helpful to you!");
 
 /* asking user for their name and location */
 var yourName;
@@ -68,38 +66,20 @@ if (theTemp != y)
 }
 
 
-
+/* using "if" with "else if" to ask the user their fave colour */
 
 /* declaring variables for colours and assigning their psychological meaning to them */
 var orange;
-orange = alert();
-
-var purple;
-purple = alert("Purple is the color of the imagination. It can be creative and individual or immature and impractical.");
-
 var green;
-green = alert("Green is the color of balance and growth. It can mean both self-reliance as a positive and possessiveness as a negative, among many other meanings. ");
-
 var brown;
-brown = alert("The color brown is a friendly yet serious, down-to-earth color that relates to security, protection, comfort and material wealth. ");
-
-var blue;
-blue = alert("Blue is the color of trust and peace. It can suggest loyalty and integrity as well as conservatism and frigidity.  ");
-
-var red;
-red = alert("Red is the color of energy, passion, action, ambition and determination. It is also the color of anger and sexual passion.");
-
-var yellow;
-yellow= alert ("yellow is the color of the mind and the intellect. It is optimistic and cheerful. However it can also suggest impatience, criticism and cowardice.");
-
-var white;
-white = alert("White is color at its most complete and pure, the color of perfection. The color meaning of white is purity, innocence, wholeness and completion. ");
-
+var purple;
+purple = ("Purple is the color of the imagination. It can be creative and individual or immature and impractical.");
 var black;
-black = alert("Black is the color of the hidden, the secretive and the unknown, creating an air of mystery. It keeps things bottled up inside, hidden from the world. ");
-
 var gray;
-gray = alert("From a color psychology perspective, gray is the color of compromise - being neither black nor white, it is the transition between two non-colors. It is unemotional and detached and can be indecisive.");
+var white;
+var yellow;
+var red;
+var blue;
 
 
 /* having user enter fave colour */
@@ -107,39 +87,54 @@ var faveColour;
 faveColour = prompt("Now lets see what you will wear today by you entering your favourite colour! But...." +
     "please only enter one of the folowing colours: orange, purple, green, blue, red, white, black, gray, or yellow and press enter." + "" + yourName + "."  +
     "so we can make this decision quite quickly bc we cant have you being late for work today! ");
-if(faveColour = orange)
+if(faveColour === "orange")
 {
-    alert(orange);
+    alert("");
 }
-if(faveColour = purple)
+else if(faveColour === "purple")
 {
-    alert(purple);
+     alert(purple);
 }
-if(faveColour = brown)
+else if(faveColour === "red")
 {
-    alert(brown);
+    alert("Red is the color of energy, passion, action, ambition and determination. It is also the color of anger and sexual passion.");
 }
-if(faveColour = blue)
+else if(faveColour === "brown")
 {
-    alert(blue);
+    alert("The color brown is a friendly yet serious, down-to-earth color that relates to security, protection, comfort and material wealth. ")
 }
-if(faveColour = green)
+if(faveColour === "blue")
 {
-    alert(green);
+    alert("Blue is the color of trust and peace. It can suggest loyalty and integrity as well as conservatism and frigidity.  ")
 }
-if(faveColour = yellow)
+ else if(faveColour === "green")
 {
-    alert(yellow);
+    alert("Green is the color of balance and growth. It can mean both self-reliance as a positive and possessiveness as a negative, among many other meanings. ")
 }
-if(faveColour = white)
+else if(faveColour === "yellow")
 {
-    alert(white);
+    alert ("yellow is the color of the mind and the intellect. It is optimistic and cheerful. However it can also suggest impatience, criticism and cowardice.")
 }
-if(faveColour = black)
+else if(faveColour === "white")
 {
-    alert(black);
+    alert("White is color at its most complete and pure, the color of perfection. The color meaning of white is purity, innocence, wholeness and completion. ")
 }
+else if(faveColour === "black")
+{
+    alert("Black is the color of the hidden, the secretive and the unknown," +
+        " creating an air of mystery. It keeps things bottled up inside, hidden from the world. ")
+}
+else if(faveColour === "gray")
+{
+    alert("From a color psychology perspective, gray is the color of compromise - " + "\n" +
+        "being neither black nor white, it is the transition between two non-colors. It is unemotional and detached and can be indecisive.");
+}
+else
+{
+    alert("Please choose from the optional colours: red, blue, orange, green, yellow, brown, black, gray, or white.");
+}
+
 
 /* goodbye */
-
-alert("Thank you for participating today," + "" + yourName + "" + goodBye + ".");
+var theEnd;
+theEnd = alert("Thank you for participating today," + "" + yourName + "" + goodBye + ".");
