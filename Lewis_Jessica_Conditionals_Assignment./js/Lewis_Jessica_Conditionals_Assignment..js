072672af6565a9c5fd35995e350fce2b0ce05ad2
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-22 21:55:35 -0400

    com 6 from macbook
    adding comments

--- Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
+++ Lewis_Jessica_Conditionals_Assignment./js/Lewis_Jessica_Conditionals_Assignment..js
@@ -2,20 +2,21 @@
  * Created by Miss_Jessica_Lynne on 7/17/15.
  */
 /* basic variables */
-var yes;
-yes = true;
-var no;
-no = false;
-var welCome;
-welCome = alert("Welcome to todays weather! We will review your daily forcast to make sure you are prepared for your day!");
+var yes;                                                            //declaring a variable
+yes = true;                                                         //assigning boolean variable for true
+var no;                                                             //declaring a variable
+no = false;                                                         //assigning boolean variable for false
+var welCome;                                                        //declaring a variable
+welCome = alert("Welcome to todays weather! We will review your " +
+    "daily forcast to make sure you are prepared for your day!");  //adding a string to the variable to welcome the user
 
 /* asking user for their name and location */
-var yourName;
-yourName = prompt("Please enter your name so I know how to properly address you.");
+var yourName;                                                                       //declaring a variable
+yourName = prompt("Please enter your name so I know how to properly address you."); // asking the user to input their name
 alert(" Thank you very much" + "," + yourName + ".");
 
 /* the weather using if/else */
-var theWeather;
+var theWeather; //declaring a variable
 theWeather = prompt(yourName + "," + "" + "Is it raining outside? y/n");
 if (theWeather == y)
 {
@@ -30,7 +31,7 @@ else
 /* temperature using if with && */
 
 
-var theTemp;
+var theTemp; //declaring a variable
 theTemp = prompt(yourName + "," + "Please enter the current temperature in numeric form...");
 
 
@@ -54,56 +55,47 @@ if ((theTemp > 75))
     console.log("Wow! It is very hot today! Lets try to loiter around the AC at work today!");
 }
 
-/* The weather using ternary conditional if/else */
-var letsConfirm;
-letsConfirm = confirm(" ok so it is definitely" + "" + theTemp + "" + "outside right now? y/n");
 
-if (theTemp != y)
-{
-    console.log("I think there was an error. Lets try this again");
-} else {
-    console.log("It is " + (theTemp >= 90 ? "HOT!" : (theTemperature > 50 ? "NICE!" : "COLD!")))
-}
 
 
 /* using "if" with "else if" to ask the user their fave colour */
 
-/* declaring variables for colours and assigning their psychological meaning to them */
-var orange;
+//declaring variables for colours and assigning their psychological meaning to them
+var orange; //declaring a variable
 orange = ("really? orange?!");
 
-var green;
+var green; //declaring a variable
 green = ("Green is the color of balance and growth. It can mean both self-reliance as a positive and possessiveness as a negative, among many other meanings. ");
 
-var brown;
+var brown; //declaring a variable
 brown = ("The color brown is a friendly yet serious, down-to-earth color that relates to security, protection, comfort and material wealth. ");
 
-var purple;
+var purple; //declaring a variable
 purple = ("Purple is the color of the imagination. It can be creative and individual or immature and impractical.");
 
-var black;
+var black; //declaring a variable
 black = ("Black is the color of the hidden, the secretive and the unknown," +
 " creating an air of mystery. It keeps things bottled up inside, hidden from the world. ");
 
-var gray;
+var gray; //declaring a variable
 gray = ("From a color psychology perspective, gray is the color of compromise - " + "\n" +
 "being neither black nor white, it is the transition between two non-colors. It is unemotional and detached and can be indecisive.");
 
-var white;
+var white; //declaring a variable
 white = ("White is color at its most complete and pure, the color of perfection. The color meaning of white is purity, innocence, wholeness and completion. ");
 
-var yellow;
+var yellow; //declaring a variable
 yellow = ("yellow is the color of the mind and the intellect. It is optimistic and cheerful. However it can also suggest impatience, criticism and cowardice.");
 
-var red;
+var red; //declaring a variable
 red = ("Red is the color of energy, passion, action, ambition and determination. It is also the color of anger and sexual passion.");
 
-var blue;
+var blue; //declaring a variable
 blue = ("Blue is the color of trust and peace. It can suggest loyalty and integrity as well as conservatism and frigidity.  ");
 
 
 /* having user enter fave colour */
-var faveColour;
+var faveColour; //declaring a variable
 faveColour = prompt("Now lets see what you will wear today by you entering your favourite colour! But...." + "\n" +
     "please only enter one of the folowing colours: orange, purple, green, blue, red, white, black, gray, or yellow " +
     "and press enter" + "," + "\n" + "" + yourName + "."  + "\n" +
@@ -156,7 +148,35 @@ else
         " black, gray, or white," + "" + yourName + "" + "\n" + "Otherwise this script will not work. Thank you!");
 }
 
+/* The weather confirm using ternary conditional if/else */
+var letsConfirm; //declaring a variable
+letsConfirm = confirm(" ok so it is definitely" + "" + theTemp + "" + "outside right now? y/n");
 
-/* goodbye */
-var theEnd;
+if (theTemp != yes)
+{
+    console.log("It is " + (theTemp >= 90 ? "HOT!" : (theTemperature > 50 ? "NICE!" : "COLD!")))
+}
+else
+{
+    console.log("I think there was an error. Lets try this again");
+}
+
+
+
+/* confirming the correct fave colour before letting user exit site */
+var letsConfirm2; //declaring a variable
+letsConfirm2 = confirm( " Now lets confirm that" + "" + faveColour + "" + "is your fave colour? yes/no and click ok.");
+if (faveColour == yes)
+{
+    console.log("So your fave colour is:" + "" + faveColour + "?" + "I hope so bc I love that colour!");
+}
+else
+{
+    console.log("I think there was an error. Lets try this again");
+}
+
+
+
+/*  initiating a goodbye */
+var theEnd; //declaring a variable
 theEnd = alert("Thank you for participating today" + "," + "" + yourName + "" + goodBye + ".");
