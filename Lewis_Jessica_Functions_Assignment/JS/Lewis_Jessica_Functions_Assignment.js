6e70bc25e69295ee461a433f78143d62ee1bf297
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-29 14:33:35 -0400

    com 3 added function for name using if/while and args and params

--- Lewis_Jessica_Functions_Assignment/JS/Lewis_Jessica_Functions_Assignment.js
+++ Lewis_Jessica_Functions_Assignment/JS/Lewis_Jessica_Functions_Assignment.js
@@ -2,50 +2,71 @@
  * Created by Miss_Jessica_Lynne on 7/28/15.
  */
 
-/*basic variables*/
-
+/*BASIC VARIABLES USED THROUGHOUT THIS SCRIPT*/
+//BOOLEAN VARIABLES
 var yes;
 yes = true;
 var no;
 no = false;
+
+
+/* VARIABLE FOR WELCOME AND GREETING*/
 var welCome;
 welCome = ("Welcome to Lucky Numbers, your hot spot for \n\n " +
 "the best random lottery and Powerball \n\n " +
 "generated numbers on the web!");
 
 
-/*greeting and asking user their name*/
-var myName = prompt("Let's begin! Please enter your name so I may better assist you:");
+/* USING A FUNCTION ASKING THE USER TO INPUT THEIR NAME*/
+var myName;
+    myName = prompt("Let's begin! Please enter your name so I may better assist you:");
 
-//assigning the function
-function yourName(username)
-{
-    var nameInput = "";
-    while (username === "")
+//assigning the function to request that the user input their name
+    function yourName(username)
     {
-        username = prompt("Dont leave this blank!\n\nPlease enter your name:");
-        if (nameInput = "")
-             {
+        var nameInput = "";
+        while (username === "")
+        {
+            username = prompt("Dont leave this blank!\n\nPlease enter your name:");
+            if (nameInput = "")
+                {
                 alert("Thank you!");
                 break;
-            }
+                }
+        }
+        return username;
     }
-    return username;
-}
-
 //calling/invoking the above function:
-myName = yourName(myName);
-alert(welCome + myName);
+        myName = yourName(myName);
+        alert(welCome + myName);
 
+/* ASKING THE USER IF THEY WANT POWERBALL OR LOTTERY NUMBERS */
+var powerLotto;
+powerLotto = prompt ("Welcome! Would you like to see your lottery numbers or pwerball number?\n\n Please enter either lottery OR powerball...");
 
+function askLotto (validlotto) //function to ask if lotto numbers are wanted
+{
+    var question = "";
+    while (validlotto === "lottery")
+    {
+         validlotto = alert("Lottery numbers it is then! Please press ok to continue....:");
 
-
-/* asking if user wants powerball or lottery*/
-
+        if (question === "powerball")
+        {
+            question = alert("Powerball numbers it is! Please press ok to continue");
+            break;
+        }
+    }
+    return validlotto;
+}
+powerLotto = askLotto(powerLotto);
+alert("Thank for entering you choice as " + powerLotto);
 
 
 
 /*running math.random()*/
+
+//LOTTO NUMBERS
 function myLotto(min, max, num)
 {
     var myNumbers = [];
@@ -60,15 +81,41 @@ function myLotto(min, max, num)
         }
     }
 }
-
-
-/* lottory random number results */
+//lottery random number results
 var randomNums;
     randomNums = myLotto(1,10,1);
-console.log("Your first random lottory number is " + randomNums +".");
-console.log("Your second random lottory number is " + randomNums +".");
-console.log("Your third random lottory number is " + randomNums +".");
-console.log("Your fourth random lottory number is " + randomNums +".");
-console.log("Your fifth random lottory number is " + randomNums +".");
-console.log("Your sixth random lottory number is " + randomNums +".");
+console.log("Your first random lottery number is " + randomNums +".");
+console.log("Your second random lottery number is " + randomNums +".");
+console.log("Your third random lottery number is " + randomNums +".");
+console.log("Your fourth random lottery number is " + randomNums +".");
+console.log("Your fifth random lottery number is " + randomNums +".");
+console.log("Your sixth random lottery number is " + randomNums +".");
 
+
+
+
+/* USING MATH.RANDOM() FOR POWERBALL NUMBERS */
+//POWERBALL NUMBERS
+
+
+
+
+
+var letsConfirm2;                                  //declaring a variable
+letsConfirm2 = prompt( " Now lets confirm that" + "," + "\n"
+    + faveColour + "\n"
+    + "is your fave colour? yes/no and click ok.");// confirming the users fave colour
+if (faveColour === yes)                            //if fave colour equals a yes from the user then do this
+{
+    alert("So your fave colour is:" + "" + faveColour + "?" + "I hope so bc I love that colour!");
+}
+else                                            // otherwise do this
+{
+    alert("I think there was an error. Lets try this again");
+}
+
+
+/*  initiating a goodbye */
+var theEnd;                                       //declaring a variable
+theEnd = alert("Thank you for participating today"
+    + "," + "" + yourName + "" + goodBye + "."); //assigning a popup alert to say thank you and goodbye to the user
