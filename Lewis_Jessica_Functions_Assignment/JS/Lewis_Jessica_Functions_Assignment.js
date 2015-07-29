8ea1dee18f8463251190523b0fd2ff4f5adf0590
Jessica Lynne <emeraldkitten82@gmail.com>
2015-07-28 20:04:31 -0400

    com 1 lotto numbers using for to return random num

--- Lewis_Jessica_Functions_Assignment/JS/Lewis_Jessica_Functions_Assignment.js
+++ Lewis_Jessica_Functions_Assignment/JS/Lewis_Jessica_Functions_Assignment.js
@@ -1,3 +1,73 @@
 /**
  * Created by Miss_Jessica_Lynne on 7/28/15.
  */
+
+/*basic variables*/
+
+
+
+
+/*greeting and asking user their name*/
+var myName = prompt("enter ur name plz:");
+
+function yourName(firstname)
+{
+    var timesClicked = 1;
+
+    while (firstname === "")
+    {
+        firstname = prompt("Dont leave this blank!\n\nPlease enter your name:");
+
+        timesClicked++;
+
+        if (timesClicked === 10)
+        {
+            console.log("Enuff nomore clicking!");
+            break;
+        }
+    }
+    return firstname;
+}
+myName = yourName(myName);
+//console.log("Hello,"+myName+".welcome
+myName=yourName(myName);
+
+
+
+//functiontogeneraterandomnumburs
+    functionrandomNumGen()
+
+
+
+/* asking if user wants powerball or lottery*/
+
+
+
+
+/*running math.random()*/
+function myLotto(min, max, num)
+{
+    var myNumbers = [];
+    {
+        {
+            for (var a = 0; a < num; a++)
+            {
+                myNumbers[a] = Math.random() * (max - min) + min;
+                myNumbers[a] = Math.round(myNumbers[a]);
+            }
+            return myNumbers;
+        }
+    }
+}
+
+
+/* lottory random number results */
+var randomNums;
+    randomNums = myLotto(1,10,1);
+console.log("Your first random lottory number is " + randomNums +".");
+console.log("Your second random lottory number is " + randomNums +".");
+console.log("Your third random lottory number is " + randomNums +".");
+console.log("Your fourth random lottory number is " + randomNums +".");
+console.log("Your fifth random lottory number is " + randomNums +".");
+console.log("Your sixth random lottory number is " + randomNums +".");
+
